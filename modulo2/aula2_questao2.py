514df2ab84b80fabf08de6283436fef63d3abccc
Franciele Freitas <francielefreitas.pdbd199@pdbomdespacho.com.br>
2025-05-12 14:31:31 -0300

    Update aula2_questao2.py

--- modulo2/aula2_questao2.py
+++ modulo2/aula2_questao2.py
@@ -1,3 +1,12 @@
+#2 - Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+'''
+Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+
+Some os valores da segunda e terceira variável e armazene em outra variável.
+
+Imprima todas as variáveis na ordem de criação e imprima também seus tipos
+'''
+
 mensagem = "o resultado é"
 valor1 = 10
 valor2 = 3.5
