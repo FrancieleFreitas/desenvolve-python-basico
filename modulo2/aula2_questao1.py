3798fbb1fa6a252e22e5ea852cc8e55a2205da65
Franciele Freitas <francielefreitas.pdbd199@pdbomdespacho.com.br>
2025-05-12 14:27:47 -0300

    Add files via upload
    Exercícios - Tipos de dados

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,19 @@
+
+#1 - Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+'''
+Armazene o valor 5 em uma variável e o valor 2 em outra variável
+
+Imprima o tipo de dados dessas duas variáveis
+
+Divida a primeira variável (com o valor 5) pela segunda variável (com o valor 2) e armazene o resultado em uma terceira variável
+
+Imprima a terceira variável e também o seu tipo
+'''
+
+a = 5
+b = 2
+print("Tipo da variável a:", type(a))
+print("Tipo da variável b:", type(b))
+c = a / b
+print("Valor da variável c:",c)
+print("Tipo da variável c:", type(c))
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,8 @@
+mensagem = "o resultado é"
+valor1 = 10
+valor2 = 3.5
+resultado = valor1 + valor2
+print(mensagem, type (mensagem))
+print(valor1, type(valor1))
+print(valor2, type(valor2))
+print(resultado, type(resultado))
